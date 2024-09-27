17ba6ef86429f7762c39865f5c411de26078b4d1
Andre <andretom15@gmail.com>
2024-09-27 15:14:06 -0700

    css caching

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./style.css";
+import "./style.css?v=1.0.2";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
